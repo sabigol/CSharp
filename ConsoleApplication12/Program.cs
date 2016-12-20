1212d7e79ccefd23308006f9203285a3e4a2589a
sabigol <golonka.sabina92@gmail.com>
2016-12-20 20:56:57 +0100

    0.02

---
+++ ConsoleApplication12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleApplication12
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var json = new WebClient().DownloadString("http://www.omdbapi.com/?s=kod");
+
+            Console.Write(json);
+          
+            dynamic stuff = JsonConvert.DeserializeObject(json);
+
+            Console.WriteLine(stuff);
+
+            Console.ReadLine();
+        }
+
+    }
+}
